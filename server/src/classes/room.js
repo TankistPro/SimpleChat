24da8d95a09b0fea3ea88bed11c192be613715ad
TankistPro <nakol.nayk@mail.ru>
2021-10-11 21:23:42 +0300

    fix

--- server/src/classes/room.js
+++ server/src/classes/room.js
@@ -4,35 +4,33 @@ class Room {
     constructor(io, socket) {
         this.io = io;
         this.socket = socket;
+
+        this.room_id = null;
     }
 
     connectUserToRoom(room_id, username) {
-        this.socket.join(room_id);
+        this.room_id = room_id;
 
-        if(!rooms.has(room_id)) {
-            rooms.set(room_id, new Map().set('users', new Map().set(this.socket.id, username)).set('messages', new Map()));
+        this.socket.join(this.room_id);
+
+        if(!rooms.has(this.room_id)) {
+            rooms.set(this.room_id, new Map().set('users', new Map().set(this.socket.id, username)).set('messages', new Map()));
         } else {
-            if(this._existUserInRoom(room_id, username)) {
+            if(this._existUserInRoom(this.room_id, username)) {
                 this.io.to(this.socket.id).emit('exist-user');
 
                 return;
             }
 
-            rooms.get(room_id).get('users').set(this.socket.id, username)
+            rooms.get(this.room_id).get('users').set(this.socket.id, username)
         }
 
         console.log(rooms);
 
-        const users = [...rooms.get(room_id).get('users').values()];
-        const messagesMap = [...rooms.get(room_id).get('messages').values()]
+        const users = [...rooms.get(this.room_id).get('users').values()];
 
-        const messages = []
+        const messages = this._parsingMessages(this.room_id);
 
-        messagesMap.forEach(m => {
-            messages.push(Object.fromEntries(m))
-        })
-
-        // console.log(messages);
         const connectUserInitData = {
             user: {
                 room_id,
@@ -43,36 +41,29 @@ class Room {
 
         this.io.to(this.socket.id).emit('successful-connect', connectUserInitData);
 
-        this.io.to(room_id).emit('connected-user', users);
+        this.io.to(this.room_id).emit('connected-user', users);
     }
 
     leaveRoom(user) {
-        console.log(user)
-        rooms.get(user.room_id).get('users').delete(this.socket.id);
+        rooms.get(this.room_id).get('users').delete(this.socket.id);
 
-        this.io.to(user.room_id).emit('disconnected-user', user.username);
+        this.io.to(this.room_id).emit('disconnected-user', user.username);
+
+        this.room_id = null;
     }
 
     sendMessage(payload) {
-        rooms.get(payload.from.room_id).get('messages').set(payload.date,
+        rooms.get(this.room_id).get('messages').set(payload.date,
             new Map().set('text', payload.text).set('date', payload.date)
-                .set('from', payload.from.username).set('room_id', payload.from.room_id));
+                .set('from', payload.from.username).set('room_id', this.room_id));
 
-        const messagesMap =  [...rooms.get(payload.from.room_id).get('messages').values()]
+        const messages = this._parsingMessages(this.room_id);
 
-        const messages = []
-
-        messagesMap.forEach(m => {
-            messages.push(Object.fromEntries(m))
-        })
-
-        console.log(rooms.get(payload.from.room_id));
-
-        this.io.to(payload.from.room_id).emit('new-message', messages);
+        this.io.to(this.room_id).emit('new-message', messages);
     }
 
     _existUserInRoom(room_id, username) {
-        const usersRoom = rooms.get(room_id).get('users').values();
+        const usersRoom = rooms.get(this.room_id).get('users').values();
 
         for (let user of usersRoom) {
             if(user === username) {
@@ -81,6 +72,18 @@ class Room {
         }
 
         return false;
+    }
+
+    _parsingMessages(){
+        const messagesMap =  [...rooms.get(this.room_id).get('messages').values()];
+
+        const messages = [];
+
+        messagesMap.forEach(m => {
+            messages.push(Object.fromEntries(m));
+        });
+
+        return messages;
     }
 }
 
